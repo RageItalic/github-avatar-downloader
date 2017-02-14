5e7100707e39733a27f83eb9d522cc48389df651
Parth Patel <parthpatelgee@gmail.com>
2017-02-14 00:33:22 +0000

    Logs body of jquery contributors

--- download_avatars.js
+++ download_avatars.js
@@ -5,8 +5,16 @@ var GITHUB_USER = "RageItalic";
 var GITHUB_TOKEN = "1d006c1e51e0de470972e1fc58c5d1b2c481c123";
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  return requestURL;
-  // ...
+  var options = {
+  url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+  // var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  request(options, function(error, response, body){
+    console.log(body);
+  });
 }
-console.log(getRepoContributors('RageItalic', 'github-avatar-downloader'));
+
+getRepoContributors('jquery', 'jquery');
