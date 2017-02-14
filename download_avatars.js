9ab8dcc12ecfe49b04b2193a7d6e62b7196a0fa2
Parth Patel <parthpatelgee@gmail.com>
2017-02-14 01:46:41 +0000

    Passed objects and loops inside callbacks

--- download_avatars.js
+++ download_avatars.js
@@ -6,15 +6,22 @@ var GITHUB_TOKEN = "1d006c1e51e0de470972e1fc58c5d1b2c481c123";
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-  url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-  headers: {
-    'User-Agent': 'request'
-  }
-};
-  // var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
   request(options, function(error, response, body){
-    console.log(body);
+    var data = JSON.parse(response.body);
+    cb(data);
   });
 }
 
-getRepoContributors('jquery', 'jquery');
+function printURLS(contributors){
+  for(i = 0; i < contributors.length; i++){
+    console.log(contributors[i]['avatar_url']);
+  }
+}
+
+getRepoContributors('jquery', 'jquery', printURLS)
